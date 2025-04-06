2abe3759249cc232107a0a0934f47d85c7c45c02
Sam <ducviet2504@gmail.com>
2025-04-07 01:08:00 +0700

    fix: update json file to deployment

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,7 @@ const configProject = configSchema.safeParse({
 })
 if (!configProject.success) {
   console.error(configProject.error.issues)
-  throw new Error('Invalid environment variables')
+  throw new Error('Các giá trị khai báo trong file .env không hợp lệ')
 }
 
 const envConfig = configProject.data
